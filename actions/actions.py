fad2cb71e9b836bbedf587c07dc0748f21dd3d58
Tony <anton.whittaker@gmail.com>
2021-03-28 19:06:38 +0200

    implemented api-call action and extended data

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,38 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+from requests import get
+from datetime import date
+YEAR = str(date.today().year - 1)
+import json
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+BASE_URL = 'http://www.openligadb.de/api'
+
+
+class ActionGetTeamRanking(Action):
+
+    def name(self) -> Text:
+        return "action_get_team_ranking"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        request_url = BASE_URL + '/getbltable/bl1/' + YEAR
+        response = get(request_url)
+        if response.ok and not response.is_redirect:
+            result = json.loads(response.content.decode())
+            table_leader = result[0]
+
+            utterance = f"""
+                {table_leader["TeamName"]} führt die Tabelle mit {table_leader["Points"]} Punkten an und hat einen Vorsprung von
+                {table_leader["Points"] - result[1]["Points"]} Punkten vor dem Tabellen-Zweiten {result[1]["TeamName"]}
+            """.replace('\n', ' ')
+
+            dispatcher.utter_message(text=utterance)
+        else:
+            dispatcher.utter_message(text="Es tut mir Leid, ich konnte die Tabelle nicht finden.")
+        return []
