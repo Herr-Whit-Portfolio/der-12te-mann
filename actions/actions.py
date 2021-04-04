c7b7be57f1d2782c2f7106146b240c6407092897
Tony <anton.whittaker@gmail.com>
2021-04-04 13:28:01 +0200

    added custom action to query past games
    added tracker mockup in tests

--- actions/actions.py
+++ actions/actions.py
@@ -104,15 +104,15 @@ class ActionGetTeamStats(FootballBotAction):
         return []
 
     def interact_with_api(self, tracker):
-        #try:
-        results = self.send_request(self.BASE_URL + self.endpoint + self.YEAR)
-        team_index = self.find_team_index(results, tracker.get_slot('team_name'))
-        team_stats = results[team_index]
-        team_stats['TeamRank'] = team_index
-        utterance = self.format_utterance(team_stats)
-        return tidy_string(utterance)
-        #except KeyError or AssertionError:
-          #  return self.apology
+        try:
+            results = self.send_request(self.BASE_URL + self.endpoint + self.YEAR)
+            team_index = self.find_team_index(results, tracker.get_slot('team_name'))
+            team_stats = results[team_index]
+            team_stats['TeamRank'] = team_index
+            utterance = self.format_utterance(team_stats)
+            return tidy_string(utterance)
+        except KeyError or AssertionError:
+            return self.apology
 
     @staticmethod
     def format_utterance(stats):
@@ -159,16 +159,19 @@ class ActionGetNextGame(FootballBotAction):
     def interact_with_api(self):
         try:
             results = self.send_request(self.BASE_URL + self.endpoint)
-            day = datetime.datetime.strptime(results[0]['MatchDateTime'], self.datetime_format).date()
-            utterance = f"Die folgenden Spiele finden am {results[0]['Group']['GroupName']} ({day}) statt:\n"
-            for result in results:
-                time = datetime.datetime.strptime(result['MatchDateTime'], self.datetime_format).time()
-                utterance += tidy_string(f"""
-                                        {time}: {result['Team1']['TeamName']} gegen {result['Team2']['TeamName']}
-                                    """) + '\n'
-            return utterance
+            return self.format_utterance(results)
         except KeyError or AssertionError:
             return self.apology
+
+    def format_utterance(self, results):
+        day = datetime.datetime.strptime(results[0]['MatchDateTime'], self.datetime_format).date()
+        utterance = f"Die folgenden Spiele finden am {results[0]['Group']['GroupName']} ({day}) statt:\n"
+        for result in results:
+            time = datetime.datetime.strptime(result['MatchDateTime'], self.datetime_format).time()
+            utterance += tidy_string(f"""
+                                        {time}: {result['Team1']['TeamName']} gegen {result['Team2']['TeamName']}
+                                    """) + '\n'
+        return utterance
 
 
 
@@ -179,3 +182,83 @@ Alle Teams einer Liga:
 
     https://www.openligadb.de/api/getavailableteams/bl1/2016
 """
+
+"""
+Spiele des 8. Spieltages der ersten Bundesliga 2016/2017:
+https://www.openligadb.de/api/getmatchdata/bl1/2016/8
+"""
+
+
+class ActionGetPreviousGames(FootballBotAction):
+    endpoint = '/getmatchdata/bl1/%(year)s/%(game_day)s'
+    apology = "Es tut mir Leid, ich konnte kein Spiel finden."
+    history_length = 3
+
+    def name(self) -> Text:
+        return "action_get_previous_games"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        utterance = self.interact_with_api(tracker)
+        dispatcher.utter_message(text=utterance)
+        return []
+
+    def interact_with_api(self, tracker):
+        try:
+            games, team_name = self.aggregate_games(tracker)
+            utterance = self.format_utterance(games, team_name)
+            return utterance
+        except KeyError or AssertionError:
+            return self.apology
+
+    def format_utterance(self, results, team_name):
+        utterance = f"In den letzten {self.history_length} Spieltagen spielte {team_name} {len(results)} Spiele:\n"
+        for result in results:
+            time = datetime.datetime.strptime(result['MatchDateTime'], self.datetime_format).time()
+            date = datetime.datetime.strptime(result['MatchDateTime'], self.datetime_format).date()
+            utterance += tidy_string(f"""
+                                        {date}, {time}: {result['Team1']['TeamName']} gegen {result['Team2']['TeamName']}; 
+                                        {result['MatchResults'][0]['PointsTeam1']} - {result['MatchResults'][0]['PointsTeam2']}
+                                    """) + '\n'
+        return utterance
+
+    def aggregate_games(self, tracker):
+        game_day = self.get_game_day()
+        team_name = self.find_full_team_name(tracker)
+        past_game_days = [game_day - i for i in range(1, self.history_length + 1)]
+        day_infos = list()
+        for day in past_game_days:
+            results = self.send_request(self.BASE_URL + self.endpoint % {'year': str(self.YEAR), 'game_day': str(day)})
+            day_info = self.filter_team_info(results, team_name)
+            day_infos.append(day_info)
+        return day_infos, team_name
+
+    def get_game_day(self):
+        next_game_endpoint = '/getmatchdata/bl1'
+        results = self.send_request(self.BASE_URL + next_game_endpoint)
+        return results[0]['Group']['GroupOrderID']
+
+    @staticmethod
+    def filter_team_info(game_day, team_name):
+        for game in game_day:
+            if game["Team1"]['TeamName'] == team_name or game["Team2"]['TeamName'] == team_name:
+                return game
+
+    def find_full_team_name(self, tracker):
+        table_endpoint = '/getbltable/bl1/' + self.YEAR
+        table = self.send_request(self.BASE_URL + table_endpoint)
+        index = self.find_team_index(table, tracker.get_slot('team_name'))
+        full_team_name = table[index]['TeamName']
+        return full_team_name
+
+    @staticmethod
+    def find_team_index(table, team_name):
+        table_names = [entry['TeamName'] for entry in table]
+        word_level_edit_distances = list()
+
+        for name in table_names:
+            word_level_edit_distances.append(min([nltk.edit_distance(word, team_name) for word in name.split(' ')]))
+
+        return word_level_edit_distances.index(min(word_level_edit_distances))
+
--- tests/test_actions.py
+++ tests/test_actions.py
@@ -1,25 +1,55 @@
 from unittest import TestCase, skip
-from actions.actions import ActionGetNextGame, ActionGetTableLeader, ActionGetTeamStats
+from actions.actions import ActionGetNextGame, ActionGetTableLeader, ActionGetTeamStats, ActionGetPreviousGames
+
+class FakeTracker():
+    def get_slot(self, key):
+        slots = {
+            'team_name': 'Bayer'
+        }
+        return slots[key]
+tracker = FakeTracker()
 
 @skip('Limit print statements')
-class TestActions(TestCase):
+class TestActionGetNextGame(TestCase):
     def test_get_next_game(self):
-        action_class = ActionGetNextGame()
-        print(action_class.interact_with_api())
+        action_object = ActionGetNextGame()
+        print(action_object.interact_with_api())
 
     def test_get_table(self):
-        action_class = ActionGetTableLeader()
-        print(action_class.interact_with_api())
+        action_object = ActionGetTableLeader()
+        print(action_object.interact_with_api())
 
-
+@skip
 class TestActionGetTeamStats(TestCase):
     def test_interact_with_api(self):
-        domain = {'team_name': 'Bayer'}
-        action_class = ActionGetTeamStats()
-        utterance = action_class.interact_with_api(domain)
+        action_object = ActionGetTeamStats()
+        utterance = action_object.interact_with_api(tracker)
         print(utterance)
         self.assertIsInstance(utterance, str)
         self.assertEqual('Bayer Leverkusen', utterance[:16])
-    @skip
+
+
     def test_find_team_index(self):
         self.fail()
+
+
+class TestActionGetPreviousGames(TestCase):
+    def test_find_team_name(self):
+        action_object = ActionGetPreviousGames()
+        name = action_object.find_full_team_name(tracker)
+        self.assertIsInstance(name, str)
+        self.assertEqual('Bayer Leverkusen', name)
+
+    def test_aggregate_games(self):
+        action_object = ActionGetPreviousGames()
+        games, _ = action_object.aggregate_games(tracker)
+        for game in games:
+            self.assertTrue(game['Team1']['TeamName'] == 'Bayer Leverkusen' or game['Team2']['TeamName'] == 'Bayer Leverkusen')
+
+    def test_interact_with_api(self):
+        action_object = ActionGetPreviousGames()
+        self.assertEqual(27, action_object.get_game_day())
+        utterance = action_object.interact_with_api(tracker)
+        print(utterance)
+        self.assertIsInstance(utterance, str)
+        #self.assertEqual('Bayer Leverkusen', utterance[:16])
